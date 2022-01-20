d4c3ec2b54f83d86256d26cfe06405d32cf4b35f
Nizar <43636043+nizarhaider@users.noreply.github.com>
2022-01-20 15:35:55 +0530

    Updated Bank Dashboard

--- apps/static/assets/js/demo.js
+++ apps/static/assets/js/demo.js
@@ -15,19 +15,7 @@ Circles.create({
 	styleText:    true
 })
 
-//Notify
-$.notify({
-	icon: 'flaticon-alarm-1',
-	title: 'Atlantis Lite',
-	message: 'Free Bootstrap 4 Admin Dashboard',
-},{
-	type: 'info',
-	placement: {
-		from: "bottom",
-		align: "right"
-	},
-	time: 1000,
-});
+
 
 // JQVmap
 $('#map-example').vectorMap(
